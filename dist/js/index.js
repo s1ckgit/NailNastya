b9d187dd3d1ce14a99cbb50535a9d901b9d70edf
Kazzzemir <s1ckwell@mail.ru>
2023-04-05 18:39:45 +0300

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+
+    const tab = document.querySelectorAll('.portfolio__list-item'),
+        list = document.querySelector('.portfolio__list'),
+        tabContent = document.querySelectorAll('.portfolio__grid');
+
+    function hideTabContent (a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    };
+
+    hideTabContent(1);
+
+    function showTabContent (b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    };
+
+    list.addEventListener('click', (event) => {
+        let target = event.target;
+
+        if (target.matches('.portfolio__list-item')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    tab.forEach((item) => {
+                        item.classList.remove('active')
+                    })
+                    target.classList.add('active')
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+})
